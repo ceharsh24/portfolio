a5494adb18d2e75a277df782ceda7dc26e2a63db
Harsh Shah <harsh.shah000@gmail.com>
2020-06-27 16:59:49 -0400

    [WIP] Init commit

---
+++ src/container/footer/footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './footer.scss';
+
+class Footer extends React.PureComponent {
+  render() {
+    return (
+      <div className="footer">
+        All rights reserved by Harsh Shah
+      </div>
+    )
+  }
+}
+
+export default Footer;
